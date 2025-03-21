1d7eb66bd993a888bb1acdeea2fecad47da507e8
balaj-ali707 <balaj.ali@ccript.com>
2025-03-22 01:39:41 +0500

    added test view in api app

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view, permissions_classes
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 
 # Create your views here.
+
+@api_view(['GET'])
+@permissions_classes([AllowAny])
+def hello_test(request):
+    return Response({"message": "Hello, World!"})
