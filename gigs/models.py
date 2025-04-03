1dc86aeccd6f650d267cdbd21df681143b1ed89c
balaj-ali707 <balaj.ali@ccript.com>
2025-04-03 22:12:25 +0500

    updated Gig model, added Contract, Application model in gigs app

--- gigs/models.py
+++ gigs/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from custom_auth.models import Artist, Venue
 
 # Create your models here.
 
@@ -8,9 +9,10 @@ class Gig(models.Model):
     startDate = models.DateTimeField()
     endDate = models.DateTimeField()
     description = models.TextField()
-    venue = models.ForeignKey('Venue', on_delete=models.CASCADE)
-    artist = models.ForeignKey('Artist', on_delete=models.CASCADE, null=True, blank=True)
-    contract = models.ImageField(upload_to='contracts/', blank=True, null=True)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    max_artist = models.IntegerField()
+    flyer_bg = models.ImageField(upload_to='gigs/flyer_bg/', blank=True, null=True)
+    flyer_text = models.CharField(max_length=255, blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -35,3 +37,36 @@ class Seat(models.Model):
     class Meta:
         verbose_name = 'Seat'
         verbose_name_plural = 'Seats'
+
+class Contract(models.Model):
+    id = models.AutoField(primary_key=True)
+    gig = models.ForeignKey('Gig', on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='gigs/contracts/', blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Contract'
+        verbose_name_plural = 'Contracts'
+
+class Application(models.Model):
+    id = models.AutoField(primary_key=True)
+    gig = models.ForeignKey('Gig', on_delete=models.CASCADE)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    is_approved = models.BooleanField(default=False)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Application'
+        verbose_name_plural = 'Applications'
+
