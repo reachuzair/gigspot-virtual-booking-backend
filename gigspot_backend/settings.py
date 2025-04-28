a911e25a804ac01023ff6668cf0aaeb54a55f9ee
balaj-ali707 <balaj.ali@ccript.com>
2025-04-29 01:08:25 +0500

    updated gigspot_backend settings.py

--- gigspot_backend/settings.py
+++ gigspot_backend/settings.py
@@ -108,7 +108,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            'hosts': [(os.getenv('REDIS_HOST', 'localhost'), 6379)],
+            'hosts': [(os.getenv('REDIS_HOST', '127.0.0.1'), 6379)],
         },
     },
 }
