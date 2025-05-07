7dc0bb735bf9b657aa673169f18c2d35fb2a716a
balaj-ali707 <balaj.ali@ccript.com>
2025-05-08 01:34:29 +0500

    added Connection model in connections app

--- connections/models.py
+++ connections/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class STATUS_CHOICES(models.TextChoices):
+    PENDING = 'pending'
+    ACCEPTED = 'accepted'
+    REJECTED = 'rejected'
+
+class Connection(models.Model):
+    artist = models.ForeignKey('custom_auth.Artist', on_delete=models.CASCADE, related_name='connections_sent')
+    connected_artist = models.ForeignKey('custom_auth.Artist', on_delete=models.CASCADE, related_name='connections_received')
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES.choices, default=STATUS_CHOICES.PENDING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = 'Connection'
+        verbose_name_plural = 'Connections'
+        
