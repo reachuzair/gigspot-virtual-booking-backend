a58e1a53121364a4edf6623bd91190e457d04db6
balaj-ali707 <balaj.ali@ccript.com>
2025-05-08 23:00:16 +0500

    modified: permission_classes for stripe_webhook

--- payments/webhooks.py
+++ payments/webhooks.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
@@ -12,6 +13,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 @api_view(['POST'])
+@permission_classes([AllowAny])
 @csrf_exempt
 def stripe_webhook(request):
     payload = request.body
