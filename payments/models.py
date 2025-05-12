d4556bb82602f4a004a2c99fb5f061e7ba3825cb
balaj-ali707 <balaj.ali@ccript.com>
2025-05-12 20:30:39 +0500

    added Ticket model in payments app

--- payments/models.py
+++ payments/models.py
@@ -19,3 +19,17 @@ class Payment(models.Model):
     foreign_key_type = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Ticket(models.Model):
+    id = models.AutoField(primary_key=True)
+    booking_code = models.CharField(max_length=255)
+    user = models.ForeignKey('custom_auth.User', on_delete=models.CASCADE)
+    gig = models.ForeignKey('gigs.Gig', on_delete=models.CASCADE)
+    qr_code = models.ImageField(upload_to='gigs/qr_codes/', blank=True, null=True)
+    checked_in = models.BooleanField(default=False)
+    checked_in_at = models.DateTimeField(null=True, blank=True, default=None)
+    checked_out = models.BooleanField(default=False)
+    checked_out_at = models.DateTimeField(null=True, blank=True, default=None)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
