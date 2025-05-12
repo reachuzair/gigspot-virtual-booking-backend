f1284fb439adfa65313970e75d142b5d1527115a
balaj-ali707 <balaj.ali@ccript.com>
2025-05-12 21:00:12 +0500

    feat: add_to_cart controller added in carts app

--- carts/views.py
+++ carts/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+from gigs.models import Gig
+from .models import CartItem
 
 # Create your views here.
+
+@api_view(['POST'])
+def add_to_cart(request):
+    user = request.user
+    gig_id = request.data.get('gig_id')
+    quantity = request.data.get('quantity', 1)
+    
+    try:
+        gig = Gig.objects.get(id=gig_id)
+        cart_item, created = CartItem.objects.get_or_create(user=user, gig=gig)
+        cart_item.quantity += quantity
+        cart_item.save()
+        return Response({'detail': 'Gig added to cart successfully.', 'item': cart_item}, status=status.HTTP_200_OK)
+    except Gig.DoesNotExist:
+        return Response({'detail': 'Gig not found.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
