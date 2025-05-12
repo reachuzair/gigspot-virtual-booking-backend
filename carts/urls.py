b62fe3f48fea448b75bff7a3f9bcbee248beb52e
balaj-ali707 <balaj.ali@ccript.com>
2025-05-12 21:38:56 +0500

    feat: remove_from_cart controller added in carts app

--- carts/urls.py
+++ carts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_to_cart, list_cart_items
+from .views import add_to_cart, list_cart_items, remove_from_cart
 
 urlpatterns = [
     path('add/', add_to_cart, name='add_to_cart'),
     path('list/', list_cart_items, name='list_cart_items'),
+    path('remove/', remove_from_cart, name='remove_from_cart'),
 ]
--- carts/views.py
+++ carts/views.py
@@ -36,3 +36,16 @@ def list_cart_items(request):
     serializer = CartItemSerializer(cart_items, many=True)
     return Response(serializer.data)
 
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def remove_from_cart(request):
+    user = request.user
+    gig_id = request.data.get('gig_id')
+    try:
+        cart_item = CartItem.objects.get(user=user, gig=gig_id, is_booked=False)
+        cart_item.delete()
+        return Response({'detail': 'Gig removed from cart successfully.'}, status=status.HTTP_200_OK)
+    except CartItem.DoesNotExist:
+        return Response({'detail': 'Gig not found in cart.'}, status=status.HTTP_404_NOT_FOUND)
+    except Exception as e:
+        return Response({'detail': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
