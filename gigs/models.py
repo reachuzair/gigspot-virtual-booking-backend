c230d87b27f1f0fb6331417bbd47c1e47e7aa936
balaj-ali707 <balaj.ali@ccript.com>
2025-05-13 16:29:21 +0500

    modified: Contract model with field is_paid in gigs app

--- gigs/models.py
+++ gigs/models.py
@@ -63,6 +63,7 @@ class Contract(models.Model):
     pdf = models.FileField(upload_to='gigs/contracts/', blank=True, null=True)
     image = models.ImageField(upload_to='gigs/contracts/', blank=True, null=True)
     price = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    is_paid = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
