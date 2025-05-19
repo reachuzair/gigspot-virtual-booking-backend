0dce5eb7a64e4fc11478786e9c5b200899e72523
hareem yashal <balaj.ali@ccript.com>
2025-05-19 20:15:22 +0500

    modified: Gig model to add fields slot_available and venue_fee

--- gigs/models.py
+++ gigs/models.py
@@ -36,6 +36,8 @@ class Gig(models.Model):
     status = models.CharField(max_length=255, choices=Status.choices, default=Status.PENDING)
     invitees = models.ManyToManyField(Artist, related_name='invited_gigs', blank=True)
     collaborators = models.ManyToManyField(Artist, related_name='collaborated_gigs', blank=True)
+    slot_available = models.BooleanField(default=True)
+    venue_fee = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     expires_at = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
