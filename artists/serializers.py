8507373d407b6b236846ff25603a7088fbb6d3c0
hamzaatccript <balaj.ali@ccript.com>
2025-05-27 20:07:11 +0500

    updated list_artist controller with user_name field in response

--- artists/serializers.py
+++ artists/serializers.py
@@ -2,7 +2,15 @@ from rest_framework import serializers
 from custom_auth.models import Artist
 
 class ArtistSerializer(serializers.ModelSerializer):
+    user_name = serializers.CharField(source='user.name', read_only=True)
+
     class Meta:
         model = Artist
-        fields = '__all__'
+        fields = [
+            'id', 'user', 'verification_docs', 'logo', 'band_name', 'band_email', 'state',
+            'performance_tier', 'subscription_tier', 'shows_created', 'active_collaborations',
+            'soundcharts_uuid', 'buzz_score', 'onFireStatus', 'connections',
+            'stripe_account_id', 'stripe_onboarding_completed', 'created_at', 'updated_at',
+            'user_name'
+        ]
         extra_kwargs = {field: {'required': True} for field in fields if field != 'id'}
