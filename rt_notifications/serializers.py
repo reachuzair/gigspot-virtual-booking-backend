e03730d660f34a3782fb22f8616e5bd2742fd9a4
Zubair Hassan <zubair.hassan@ccript.com>
2025-05-30 18:08:29 +0500

    Notifications Routes Fixed

--- rt_notifications/serializers.py
+++ rt_notifications/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Notification
 
+
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Notification
-        fields = ['id', 'notification_type', 'title', 'message', 'is_read', 'created_at']
+        fields = ['id', 'notification_type', 'title',
+                  'message', 'is_read','created_at', 'recipient']
         read_only_fields = ['created_at']
+
+    
