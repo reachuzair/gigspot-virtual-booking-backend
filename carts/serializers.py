89c594ffe4d9cd54790fdd4e99bce9c2bce15637
Zubair Hassan <zubair.hassan@ccript.com>
2025-06-17 22:10:05 +0500

    - Updated CartItemSerializer to nest GigSerializer - Ensures that each cart item includes full gig information in the /api/cart/list endpoint

--- carts/serializers.py
+++ carts/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
+
+from gigs.serializers import GigSerializer
 from .models import CartItem
 
 class CartItemSerializer(serializers.ModelSerializer):
+    gig = GigSerializer(read_only=True)
     class Meta:
         model = CartItem
         fields = '__all__'
