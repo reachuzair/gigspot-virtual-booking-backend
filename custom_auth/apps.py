7fb369c9c9c8c6d5e6b60652f9b1e06a20bbc0cb
Shoaib <shoaib.akbar@ccript.com>
2025-06-25 18:21:18 +0500

    auto update venue tier added

--- custom_auth/apps.py
+++ custom_auth/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AuthConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'custom_auth'
+    
+    def ready(self):
+        # Import signals to register them
+        import custom_auth.signals
