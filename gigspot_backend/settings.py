8ebc47de96bc50de027396ae533ded71ac74015d
Zubair Hassan <zubair.hassan@ccript.com>
2025-07-02 00:25:01 +0500

    fixed the image url in Gig detail response

--- gigspot_backend/settings.py
+++ gigspot_backend/settings.py
@@ -234,11 +234,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files configuration
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
@@ -328,4 +328,3 @@ LOGGING = {
         },
     },
 }
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
